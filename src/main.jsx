5b23131b91df0415f717eea04cfdf0c4069baafa
Kashishjangid <kashishjangid71363@gmail.com>
2025-11-18 23:22:10 +0530

    Initial push

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import SingleBlog from './components/SingleBlog.jsx';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/blog/:id" element={<SingleBlog />} />
-      </Routes>
-    </Router>
-  
+    <App />
   </StrictMode>,
 )
